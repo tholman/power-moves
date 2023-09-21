e84203df59870d4b93475dbe0b9d5d73c77223fe
Tim Holman <timothy.w.holman@gmail.com>
2023-09-20 21:12:34 -0400

    powermoves

--- powermoves/powermovesApp.swift
+++ powermoves/powermovesApp.swift
@@ -8,10 +8,30 @@
 import SwiftUI
 
 @main
-struct powermovesApp: App {
+struct PowerMovesApp: App {
+    
+    @State var isInserted = true
+    @State var colorIndex = 0
+    
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        
+        let colors: [NSColor] = [.red, .blue, .orange]
+        MenuBarExtra(isInserted: $isInserted) {
+            Button("Shift") {
+                colorIndex = (colorIndex+1) % colors.count
+            }
+        } label: {
+            let configuration = NSImage.SymbolConfiguration(pointSize: 17, weight: .light)
+                        .applying(.init(paletteColors: [colors[colorIndex]]))
+            let image = NSImage(systemSymbolName: "battery.0", accessibilityDescription: nil)
+            let updateImage = image?.withSymbolConfiguration(configuration)
+            Image(nsImage: updateImage!)
         }
+    }
+}
+
+struct AppMenu: View {
+    var body: some View {
+        Text("App Menu Item")
     }
 }
