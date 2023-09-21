675c59314ceacfdf6ebcf7b36a9aaacae9805f36
Tim Holman <timothy.w.holman@gmail.com>
2023-09-20 20:04:47 -0400

    Initial Commit

---
+++ powermoves/powermovesApp.swift
@@ -0,0 +1,17 @@
+//
+//  powermovesApp.swift
+//  powermoves
+//
+//  Created by Tim Holman on 9/20/23.
+//
+
+import SwiftUI
+
+@main
+struct powermovesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
